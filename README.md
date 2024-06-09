50ff3247ab831e34ae43f6a6ec43c53bd0b3c2a4
geraldo leite <repos.admin@singularitya.com>
2024-06-09 20:50:50 -0300

    version 2

--- README.md
+++
@@ -1,2 +0,0 @@
-# corporate-web
-Web page.
--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  setTimeout(function() {
-      var section = document.getElementById('section');
-      section.classList.remove('hidden');
-  }, 1000); // Aguarda 1000 milissegundos (1 segundo) antes de remover a classe
-});
